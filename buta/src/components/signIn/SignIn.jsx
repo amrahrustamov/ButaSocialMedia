06a46a8607edc33b55a435fc7328b8fa944227e6
Amrah Rustamov <amrahrustamov94@gmail.com>
2024-01-03 11:06:38 +0400

    some names changed and added shadow to box

--- buta/src/components/signIn/SignIn.jsx
+++ buta/src/components/signIn/SignIn.jsx
@@ -1,45 +1,50 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { CiUser } from "react-icons/ci";
+import { PiLockKeyFill } from "react-icons/pi";
+import { FcMindMap } from "react-icons/fc";
 
 const SignIn = () => {
   return (
     <div className="signIn">
         <div className="container">
-            <div className="justify-content-center row max-height">
-                <div className="col-md-8 col-lg-6 col-xl-5 max-height d-flex row">
-                    <div className="topPartContainer mt-4 d-flex align-items-center justify-content-center">
-                        <div className="topPart text-center">
-                            <a href="/" className='text-muted'>Buta</a>
+            <div className="justify-content-center row">
+                <div className="col-md-8 col-lg-6 col-xl-5 conBoxes">
+                    <div className="topPartContainer text-center">
+                        <div className="topPart text-center p-2">
+                            <a href="/" className='emblemOfSite'><FcMindMap />Buta<FcMindMap /></a>
                             <h4>Sign in</h4>
                             <p>Sign in to continue to Buta</p>
                         </div>
                     </div>
                     <div className="signInSection">
                         <div className="p-4 card-body">
-                            <div className="p-3">
-                                <form action="" className='d-flex row'>
-                                    <div className="mb-3">
-                                        <label className='form-label'>Username</label>
-                                        <div className="mb-3">
-                                            <CiUser className='m-3'/>
-                                            <input type="text" name="email" id="email" placeholder='Enter email'/>
+                            <div className="inCardBody">
+                                <form action="" className='formBox'>
+                                    <div className="mb-3 username">
+                                        <label className='form-label'>Email</label>
+                                        <div className="mb-3 conInput">
+                                            <div className="conLogo">
+                                                <CiUser className='userLogo'/>
+                                            </div>
+                                            <input type="text" name="email" id="email" placeholder='Enter email' className='input-with-margin-left'/>
                                         </div>
                                     </div>
-                                    <div className="mb-3">
+                                    <div className="mb-3 password">
                                         <div className="float-end">
-                                            <a href="/" className='font-size-13 text-muted'>Forget password?</a>
+                                            <a href="/" className='font-size-13 text-muted forgetPassword'>Forget password?</a>
                                         </div>
                                         <label className='form-label'>Password</label>
-                                        <div className="mb-3">
-                                            <CiUser className='m-3'/>
-                                            <input type="password" name="password" id="password" placeholder='Enter password'/>
+                                        <div className="mb-3 conInput">
+                                            <div className="conLogo">
+                                                <PiLockKeyFill className='passwordLogo'/>
+                                            </div>
+                                            <input type="password" name="password" id="password" placeholder='Enter password' className='input-with-margin-left'/>
                                         </div>
 
                                     </div>
-                                    <div className="mb-4 float-start">
+                                    <div className="mb-4 float-start checkBox">
                                         <input type="checkbox" id="remember-check" className='form-check-input'/>
                                         <label for="remember-check" className='form-check-label'>Remember me</label>
                                     </div>
@@ -50,8 +55,8 @@ const SignIn = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="bottomPart mt-1 text-center">
-                        <p>Don't have an account ? <a href="/" className='font-weight-medium text-primary'>Signup now</a></p>                       
+                    <div className="bottomPart text-center">
+                        <p>Don't have an account ? <a href="/" >Signup now</a></p>                       
                         <p>
                         © 2024 Buta. Created by Amrah Rustamov
                         </p>
