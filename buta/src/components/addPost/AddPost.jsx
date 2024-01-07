4cb50fcbd98842173ae9747949785f713d770999
Amrah Rustamov <amrahrustamov94@gmail.com>
2024-01-07 21:27:32 +0400

    add post component updated

--- buta/src/components/addPost/AddPost.jsx
+++ buta/src/components/addPost/AddPost.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../App.css'
-import { Link } from 'react-router-dom'
+import { FcGallery } from "react-icons/fc";
 
 const AddPost = () => {
   return (
@@ -8,15 +8,12 @@ const AddPost = () => {
         <div className="containerAddPost">
                 <div className="topPartOfAddPost">
                     <div className="profileImg">
-                        <Link>
                             <img src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg" alt="logo" />
-                        </Link>
                     </div>
                     <div className="input">
                         <input type="text" placeholder='Add story'/>
                     </div>
                 </div>
-            <div className="bottomPartOfAddPost"></div>
         </div>
     </div>
   )
