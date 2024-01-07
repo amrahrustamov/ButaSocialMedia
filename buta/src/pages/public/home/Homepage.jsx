f17d25a2d97e69f6f16feb88716ed52c2336f96d
Amrah Rustamov <amrahrustamov94@gmail.com>
2024-01-07 20:51:21 +0400

    created top part of addPost

--- buta/src/pages/public/home/Homepage.jsx
+++ buta/src/pages/public/home/Homepage.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Header from '../../../components/header/Header'
+import AddPost from '../../../components/addPost/AddPost'
 import '../../../App.css'
 
 const Homepage = () => {
   return (
     <div className='homepage'>
       <Header />
-      <main className='text-center'>
-        This is homepage
+      <main>
+        <div className="leftPartOfHomePage">left part</div>
+        <div className="middlePartOfHomePage"><AddPost /></div>
+        <div className="rightPartOfHomePage">right part</div>
       </main>
     </div>
   )
 }
-
 export default Homepage
