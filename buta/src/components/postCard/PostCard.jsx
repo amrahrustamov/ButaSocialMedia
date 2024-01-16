3382a6b7c8cc9296a2c208dec2b8f199037d64fc
Amrah Rustamov <amrahrustamov94@gmail.com>
2024-01-16 21:02:32 +0400

    empty

--- buta/src/components/postCard/PostCard.jsx
+++ buta/src/components/postCard/PostCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../App.css'
 import { Link } from 'react-router-dom';
 import { IoEarthSharp } from "react-icons/io5";
@@ -7,18 +7,24 @@ import { SlOptions } from "react-icons/sl";
 import { AiOutlineLike } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
 import { PiShareFat } from "react-icons/pi";
+import { HiOutlineClipboardList } from "react-icons/hi";
 
-const PostCard = () => {
+const PostCard = (props) => {
+  const { name , picture} = props.item;
+  useEffect(()=> {
+    console.log(props.item);
+  }, [])
+  
   return (
     <div className='postCard'>
         <div className="topPostCard d-flex">
               <div className="leftOfTopPost d-flex">
                 <div className="img">
-                    <img src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg" alt="profile" />
+                    <img src={picture.large} alt="profile" />
                 </div>
                 <div className="timeAndName">
                   <div className="name d-flex">
-                    <p>Mr Jackhson Sanchez</p>
+                    <p>{props.item.name.title} {name.first} {name.last}</p>
                     <Link className='follow'>Follow</Link>
                   </div>
                   <div className="time">
@@ -40,7 +46,11 @@ const PostCard = () => {
             <img src="https://img.freepik.com/free-photo/buta-shape-red-jewelry-pin_114579-12101.jpg" alt="postImg" />
           </div>
         </div>
-        <div className="about"></div>
+        <div className="about">
+          <div className="containerAbout">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis delectus vero, inventore optio nam alias dicta repellendus, dolorem doloremque assumenda et non dignissimos nesciunt incidunt consequuntur similique, suscipit officia iste.</p>
+          </div>
+        </div>
         <div className="bottomPostCard">
           <div className="countOfActivites">
             <div className="leftPart"><AiOutlineLike /><span>15K</span></div>
@@ -50,9 +60,10 @@ const PostCard = () => {
             </div>
           </div>
           <div className="buttons">
-              <Link><AiOutlineLike />Like</Link>
-              <Link><FaRegComment />Comment</Link>
-              <Link><PiShareFat />Share</Link>
+              <Link><AiOutlineLike /></Link>
+              <Link><FaRegComment /></Link>
+              <Link><PiShareFat /></Link>
+              <Link><HiOutlineClipboardList /></Link>
           </div>
 
         </div>
--- buta/src/pages/public/home/Homepage.jsx
+++ buta/src/pages/public/home/Homepage.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../../components/header/Header'
 import AddPost from '../../../components/addPost/AddPost'
 import '../../../App.css'
 import PostCard from '../../../components/postCard/PostCard'
+import axios from 'axios'
 
 const Homepage = () => {
+  const [posts, setPosts] = useState();
+
+  useEffect(() =>{
+    const getPosts = async () => {
+        await axios.get('https://randomuser.me/api/?results=50')
+        .then(response => setPosts(response.data.results))
+        .catch(err => console.log(err))
+    }
+    try{
+      if(!posts){
+        getPosts();
+        console.log('beli');
+      }
+    }
+      catch{
+
+      }
+    
+  },[posts]);
   return (
     <div className='homepage'>
       <Header />
@@ -13,7 +33,15 @@ const Homepage = () => {
         <div className="middlePartOfHomePage">
           <AddPost />
             <div className="postCards">
-              <PostCard />
+              {
+                posts && posts.map((item, index) => {
+                  return (
+                    <PostCard key={index} item={item}/>
+                  )
+                })
+              }
+              
+            
             </div>
         </div>
         <div className="rightPartOfHomePage">right part</div>
