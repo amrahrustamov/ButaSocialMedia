263fd7906717a652429091ec96aab0a5c74793b8
Amrah Rustamov <amrahrustamov94@gmail.com>
2024-02-16 11:55:23 +0400

    added exit component

--- buta/src/components/header/Header.jsx
+++ buta/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState } from 'react'
 import '../../App.css'
 import { FcMindMap } from 'react-icons/fc'
 import { CiSearch } from 'react-icons/ci'
@@ -8,9 +8,14 @@ import { LiaUserFriendsSolid, LiaLayerGroupSolid } from "react-icons/lia";
 import { CgMenuGridO } from "react-icons/cg";
 import { BiSolidMessageDetail } from "react-icons/bi";
 import { IoMdNotifications } from "react-icons/io";
+import Exit from '../exit/Exit';
 
 const Header = () => {
 
+  const [exit, setExit] = useState(false);
+  const  handleClick = () => {
+    setExit(!exit)
+  }
   return (
     <header>
       <div className="containerHeader">
@@ -21,7 +26,7 @@ const Header = () => {
             <div className="col-lx-3 col-lg-3 col-md-4 col-sm-4 middlePartOfHeader">
                 <div className='containerOfMiddlePart'>
                     <div id='friendsStory'><NavLink to={'/friends_story'}><LiaUserFriendsSolid /></NavLink></div>
-                    <div id='allStory'><NavLink to={'/all_story'}><AiOutlineHome /></NavLink></div>
+                    <div id='allStory'><NavLink to={'/home'}><AiOutlineHome /></NavLink></div>
                     <div id='pageStory'><NavLink to={'/group_story'}><LiaLayerGroupSolid /></NavLink></div>   
                 </div>
                  
@@ -31,8 +36,9 @@ const Header = () => {
                 <div className='menu'><Link><CgMenuGridO /></Link></div>
                 <div className='messages'><Link><BiSolidMessageDetail /></Link></div>
                 <div className='notifications'><Link><IoMdNotifications /></Link></div>
-                <div className='profile'><Link><img src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg" alt="logo" /></Link></div>
+                <div className='profilePhoto'><Link onClick={handleClick}><img src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg" alt="logo" /></Link></div>
               </div>
+              <Exit exit={exit}/>
             </div>
       </div>
     </header>
